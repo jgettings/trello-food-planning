8a049a1d7777c7f9dcf231e4ee03087fed211d0a
Jen Gettings <jennifer.wystup@gmail.com>
2020-12-16 20:09:48 -0500

    more

--- public/estimate.js
+++ public/estimate.js
@@ -1,5 +1,21 @@
+/* global TrelloPowerUp */
+
 var t = TrelloPowerUp.iframe();
 
-t.render(function() {
-  t.sizeTo("#estimate").done();
+window.estimate.addEventListener('submit', function(event){
+  event.preventDefault();
+  return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+  .then(function(){
+    t.closePopup();
+  });
+});
+
+t.render(function(){
+  return t.get('card', 'shared', 'estimate')
+  .then(function(estimate){
+    window.estimateSize.value = estimate;
+  })
+  .then(function(){
+    t.sizeTo('#estimate').done();
+  });
 });
