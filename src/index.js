f57b823419840c95b090c240bb017a1483cc66ce
Jen Gettings <jennifer.wystup@gmail.com>
2021-01-29 21:19:10 -0500

    board-buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createShoppingList } from './shoppingList';
+
 TrelloPowerUp.initialize({
   'card-buttons': () => ([{
     icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
@@ -9,4 +11,12 @@ TrelloPowerUp.initialize({
       });
     },
   }]),
+  'board-buttons': () => ([
+    {
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Create Shopping List',
+      condition: 'edit',
+      callback: createShoppingList,
+    },
+  ]),
 });
---
+++ src/shoppingList/index.js
@@ -0,0 +1,3 @@
+export const createShoppingList = (t) => {
+  console.log(t);
+};
