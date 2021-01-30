b3318ecc47fea9fb77c902b3c3564c71f2efc623
Jen Gettings <jennifer.wystup@gmail.com>
2021-01-29 21:25:39 -0500

    ok now

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ TrelloPowerUp.initialize({
     {
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Create Shopping List',
-      condition: 'edit',
-      callback: () => {
-        console.log('hi!');
-      },
     },
   ]),
 });
