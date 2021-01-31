619910253c2cd89d921d04983567a47c23819a01
Jen Gettings <jennifer.wystup@gmail.com>
2021-01-30 22:04:14 -0500

    dumb

--- server.js
+++ server.js
@@ -24,18 +24,18 @@ console.log(process.cwd());
 console.log(__dirname);
 
 /* eslint-disable */
-function getFiles (dir, files_){
-  files_ = files_ || [];
-  var files = fs.readdirSync(dir);
-  for (var i in files){
-      var name = dir + '/' + files[i];
-      if (fs.statSync(name).isDirectory()){
-          getFiles(name, files_);
-      } else {
-          files_.push(name);
-      }
-  }
-  return files_;
-}
+// function getFiles (dir, files_){
+//   files_ = files_ || [];
+//   var files = fs.readdirSync(dir);
+//   for (var i in files){
+//       var name = dir + '/' + files[i];
+//       if (fs.statSync(name).isDirectory()){
+//           getFiles(name, files_);
+//       } else {
+//           files_.push(name);
+//       }
+//   }
+//   return files_;
+// }
 
-console.log(getFiles(__dirname));
+// console.log(getFiles(__dirname));
